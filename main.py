3d1fdfd9dcb3f101c615424c9580cc62c47cd377
A <201318652@ingenieria.usac.edu.gt>
2022-07-05 02:30:13 -0600

    asdlj

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, mean_squared_error, r2_score
-from sklearn.decomposition import PCA
+from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # :::::::::::::::::::      Header      :::::::::::::::::::
 
@@ -44,47 +41,31 @@ st.write("""
 
 # :::::::::::::::::::      Functions      :::::::::::::::::::
 
-def add_parameter_ui(clf_name, headers):
-    params = dict()
-    if clf_name == "KNN":
-        K = st.sidebar.slider("K", 1, 15)
-        params["K"] = K
-    elif clf_name == "SVM":
-        C = st.sidebar.slider("C", 0.01, 10.0)
-        params["C"] = C
-    elif clf_name == "Regresión lineal":
-        # st.write(headers.iloc[1].tolist())
-        params["x"] = st.selectbox("Elige la columna para el eje X", headers)
-        params["y"] = st.selectbox("Elige la columna para el eje y", headers)
-    else:
-        max_depth = st.sidebar.slider("max_depth", 2, 15)
-        n_estimators = st.sidebar.slider("n_estimators", 1, 100)
-        params["max_depth"] = max_depth
-        params["n_estimators"] = n_estimators
-    return params
-
-
-def get_classifier(clf_name, params, data):
+def get_algoritmo(clf_name, headers, data):
     if clf_name == "Regresión lineal":
-        x_selected = params["x"]
-        y_selected = params["y"]
-        linear_regresion(x_selected, y_selected, data)
-    elif clf_name == "KNN":
-        KNeighborsClassifier(n_neighbors=params["K"])
-    elif clf_name == "SVM":
-        SVC(C=params["C"])
+        x_selected = st.selectbox("Elige la columna para el eje X", headers)
+        y_selected = st.selectbox("Elige la columna para el eje y", headers)
+        pred = st.text_input("Ingresa el valor a predecir")
+        linear_regresion(x_selected, y_selected, int(pred), data)
+    elif clf_name == "Regresión polinomial":
+        x_selected = st.selectbox("Elige la columna para el eje X", headers)
+        y_selected = st.selectbox("Elige la columna para el eje y", headers)
+        pred = st.text_input("Ingresa el valor a predecir")
+        # array from 0 to 25
+        grado = st.slider("grado", 1, 20)
+        polinomial_reg(x_selected, y_selected, grado, int(pred), data)
     else:
-        RandomForestClassifier(n_estimators=params["n_estimators"], max_depth=params["max_depth"], random_state=1234)
+        st.write("xd?")
 
 
 #   :::::::::::::::::::      Algoritmos      :::::::::::::::::::
 
 # |||||||||||||     Regresión lineal    |||||||||||||
 
-def linear_regresion(x_col, y_col, data):
+def linear_regresion(x_col, y_col, pred, data):
     X = np.asarray(data[x_col]).reshape(-1, 1)
     Y = data[y_col]
-
+    # Linear
     linear_regression = LinearRegression()
     linear_regression.fit(X, Y)
     Y_pred = linear_regression.predict(X)
@@ -94,14 +75,38 @@ def linear_regresion(x_col, y_col, data):
     st.write("Coef: ", linear_regression.coef_)
     st.write("R2: ", r2_score(Y, Y_pred))
 
-    Y_new = linear_regression.predict([[2050]])
+    Y_new = linear_regression.predict([[pred]])
     st.write("Prediccion: ", Y_new)
 
     plt.scatter(X, Y)
     plt.colorbar()
     plt.plot(X, Y_pred, color='red')
     st.pyplot()
-    # plt.show()
+
+
+# |||||||||||||     Regresión polinomial    |||||||||||||
+
+def polinomial_reg(x_col, y_col, grado, pred, data):
+    X = np.asarray(data[x_col]).reshape(-1, 1)
+    Y = data[y_col]
+    # Polynomial
+    poly = PolynomialFeatures(degree=grado)
+    X_trans = poly.fit_transform(X)
+    # Linear
+    linear_regression = LinearRegression()
+    linear_regression.fit(X_trans, Y)
+    Y_pred = linear_regression.predict(X_trans)
+
+    # st.write(Y_pred)
+    st.write("Error medio: ", np.sqrt(mean_squared_error(Y, Y_pred, squared=False)))
+    st.write("R2: ", r2_score(Y, Y_pred))
+
+    Y_new = linear_regression.predict(poly.fit_transform([[int(pred)]]))
+    st.write("Predicción:", Y_new)
+
+    plt.scatter(X, Y)
+    plt.plot(X, Y_pred, color='purple')
+    st.pyplot()
 
 
 #   ::::::::::::::::::::::   File upload    ::::::::::::::::::::::
@@ -127,50 +132,13 @@ if uploaded_file is not None:
         data_frame = None
 
     if data_frame is not None:
+        # data_frame = data_frame.replace('', np.nan, regex=True)
+        # data_frame = data_frame.fillna(0)
         headers = data_frame.columns
-        # st.write("Cabeceras: ")
-        # st.write(headers)
-
-        # dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine Dataset"))
-        # st.write(dataset_name)
-
         # --------      Eligiendo el algoritmo      --------
-        classifier_name = st.sidebar.selectbox("Elige el algoritmo",
-                                               ("KNN", "SVM", "Random Forest", "Regresión lineal"))
+        classifier_name = st.sidebar.selectbox("Elige el algoritmo", ("Regresión lineal", "Regresión polinomial"))
 
         st.write(""" ## Parametrización """)
-        params = add_parameter_ui(classifier_name, headers)
-
-        get_classifier(classifier_name, params, data_frame)
-
-        # X, y = get_dataset(data_frame)
-        # st.write("Shape of dataset:", X.shape)
-        # st.write("Number of classes:", len(np.unique(y)))
-        #
-        # # --------      Classification      --------
-        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
-        # clf.fit(X_train, y_train)
-        # y_pred = clf.predict(X_test)
-        #
-        # acc = accuracy_score(y_test, y_pred)
-        # st.write(f"classifier = {classifier_name}")
-        # st.write(f"accuracy = {acc}")
-        #
-        # # --------      PLOT        --------
-        # pca = PCA(2)
-        # X_projected = pca.fit_transform(X)
-        #
-        # x1 = X_projected[:, 0]
-        # x2 = X_projected[:, 1]
-        #
-        # fig = plt.figure(figsize=(8, 8))
-        # plt.scatter(x1, x2, c=y, alpha=0.8, cmap="rainbow")
-        # plt.xlabel("PCA 1")
-        # plt.ylabel("PCA 2")
-        # # plt.title(f"{dataset_name} - {classifier_name}")
-        # plt.colorbar()
-        # st.set_option('deprecation.showPyplotGlobalUse', False)
-        # # plt.show()
-        # st.pyplot()
+        get_algoritmo(classifier_name, headers, data_frame)
 else:
     st.write("Archivo invalido")
